ef1db4c90c539a8f83e187b43a558a62660cf043
unknown <aatir.farooqui@gmail.com>
2018-01-23 17:48:02 +0500

    starting rest

--- server.js
+++ server.js
@@ -4,3 +4,10 @@ const app = express();
 app.listen(3000,function(){
 	console.log('Listening to PORT 3000');
 })
+
+
+app.get('/', function (request, response) {
+  console.log('Index hit');
+})
+
+
