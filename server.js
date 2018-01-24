a253b40d92f76dcd09a5ae0cb59ce1a2ac333b6c
unknown <aatir.farooqui@gmail.com>
2018-01-24 12:26:54 +0500

    rest + mongo connected

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
 const app = express();
 
-app.listen(3000,function(){
-	console.log('Listening to PORT 3000');
+var db;
+
+MongoClient.connect('mongodb://aatir:rubikscub3@ds213688.mlab.com:13688/quotes', (err, database) => {
+	if (err) {
+		return console.log(err);
+	}
+	db = database
+	
+	app.listen(3000, () => {
+		console.log('listening on 3000');
+	})
 })
 
+app.use(bodyParser.urlencoded({extended: true}))
 
-app.get('/', function (request, response) {
-  console.log('Index hit');
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/index.html')
+})	
+
+app.post('/quotes', (req, res) => {
+	res.send(req.body);
 })
 
 
